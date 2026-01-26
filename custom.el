df721005822de864c3003b2cdb0c53671528e419
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2026-01-26 08:59:56 -0500

    Changes on KILO - 2026-01-26 08:59

--- custom.el
+++ custom.el
@@ -10,12 +10,37 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(ample-zen-theme backline badger-theme beacon breadcrumb buffer-move buttercup
+                     cape centaur-tabs cider citre clojure-ts-mode
+                     colonoscopy-theme colorful-mode consult-gh-embark
+                     consult-gh-forge consult-lsp consult-projectile
+                     consult-todo consult-yasnippet corfu darktooth-theme
+                     dashboard deft dired-auto-readme dired-gitignore dirvish
+                     doom-modeline doom-themes drag-stuff ef-themes evil-avy
+                     evil-collection evil-commentary evil-iedit-state evil-lion
+                     evil-multiedit evil-numbers flycheck-clj-kondo
+                     flycheck-hl-todo flycheck-posframe general git-modes
+                     harpoon helpful highlight-indent-guides
+                     jetbrains-darcula-theme lsp-treemacs lsp-ui magit-org-todos
+                     magit-todos major-mode-hydra marginalia multi-term
+                     nerd-icons-completion nerd-icons-corfu nerd-icons-dired
+                     nnhackernews nushell-ts-mode ob-powershell ob-rust
+                     orderless org-cliplink org-clock-today org-context
+                     org-download org-gcal org-kanban org-mem org-modern
+                     org-modern-indent org-ql org-recur org-roam-ui
+                     org-transclusion outline-yaml powershell projectile-ripgrep
+                     rainbow-delimiters restart-emacs sideline-flycheck
+                     sublime-themes surround system-packages term-projectile
+                     toggle-term treemacs-evil treemacs-nerd-icons
+                     treemacs-projectile treesit-auto undo-fu vertico-posframe
+                     which-key-posframe winum writeroom-mode yasnippet-capf
+                     yequake zenburn-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background unspecified)))))
+ '(default ((t (:background nil)))))
 
 ;;; custom.el ends here
